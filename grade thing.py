b0d12c0f87f14fa9d406455bd54cfd2050f09306
pru88 <67837568+pru88@users.noreply.github.com>
2020-07-04 18:24:18 +0100

    Add files via upload

---
+++ grade thing.py
@@ -0,0 +1,9 @@
+score= int(input("what is your score  :"))
+if score > 90:
+    print("your grade is A")
+elif score > 80:
+    print("your grade is B")
+elif score > 70:
+    print("your grade is C")
+else:
+    print("your grade is nothing. You failed")
